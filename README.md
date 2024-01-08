41fc64ba5b1920e465f936bb932988f47d795e20
Moein Alizadeh <truemoein@gmail.com>
2024-01-08 09:43:35 +0330

    change everything

--- README.md
+++
@@ -1,6 +0,0 @@
-# TrueMoein Personal Blog at
-
-## [TrueMoein.ir](https://truemoein.ir)
-
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/d0ea2053-c584-48db-8bde-83559d578e2c/deploy-status)](https://app.netlify.com/sites/truemoein/deploys)
---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+   
+        AOS.init({
+            offset: 100,
+            once: true,
+            duration: 800,
+            throttleDelay: 50,
+            disable: window.innerWidth < 768
+        });
+   
+
+});
